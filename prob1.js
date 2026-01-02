15f4ead2e6a52611f0763420766487797ae8429a
nehabmuhammed <128158357+nehabmuhammed@users.noreply.github.com>
2026-01-02 14:47:25 +0530

    Fix syntax for even/odd check in prob1.js

--- prob1.js
+++ prob1.js
@@ -2,3 +2,7 @@
 
 let num = 3;
 num % 2 == 0 ?console.log(num,"Is even"):console.log(num,"Is odd")
+// Write a program to check if a given number is even or odd.
+
+let num = 3;
+num % 2 == 0 ?console.log(num,"Is even"):console.log(num,"Is odd")
